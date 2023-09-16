da56725f0324390a039c3478cf0858edd3d21262
Abdelmalek Mohamed_Seghir <135925192+med1seghir5@users.noreply.github.com>
2023-09-16 22:00:19 +0100

    Add files via upload

--- src/Components/My projects.js
+++ src/Components/My projects.js
@@ -9,7 +9,7 @@ export const Projects =()=>{
                     <img src="https://i.pinimg.com/564x/a2/1f/63/a21f638517f349c1f84bce2cd1c3e731.jpg" id="picbio"/>
                 </div>
                 <div className="col-9 col-sm-12" id="bio">
-                    <h4 id="bio1"> Hello my name is Abdelmalek Mohamed seghir, i have 19 years old, am Computer-science student (second years), in univeristy of Batna 2, am a new front-end developer, am member and web-master in SPE univeristy of Batna2. </h4>
+                    <h4 id="bio1"> Hello my name Abdelmalek Mohamed sghir, i am 19 years old and I am a second year computer science student in university of batna 2 and a new front-end developer. </h4>
                 </div>
            </div>
         </div>
